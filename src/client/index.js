097157cac235b5799d36ef964a4e4bf7f91cbfc0
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-07-12 16:33:44 +0200

    update README.md and add service workers

--- src/client/index.js
+++ src/client/index.js
@@ -10,3 +10,13 @@ const form = document.getElementById("form");
 form.addEventListener("submit", (event) => {
     handleSubmit(event);
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            title: 'Progressive Web Application',
+            template: "./src/client/views/index.html",
+            filename: "index.html",
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
+    ]
+}
